5d28d1f4c722baf956353960da7f07693ce2f778
BackDad <manuk_1122@mail.ru>
2023-07-05 23:16:47 +0300

    Добавлено: новые окна,функции,обработки ввода,сведениея об ошибке

--- main.py
+++ main.py
@@ -1,58 +1,143 @@
+# *******************************************************************Refactored*********************************
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QMessageBox
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QTableWidgetItem, QTableView, QHeaderView
 from PyQt5.uic import loadUi
 import pymysql
 from config import host, user, password, db_name
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
 
-def Connect_to_db(self):
-    try:
-        connection = pymysql.connect(
-            host=host,
-            port=3306,
-            password=password,
-            database=db_name,
-            cursorclass=pymysql.cursors.DictCursor,
-            user=user
-        )
-        self.Result_Area.setText("Successfully Connected...")
-        print("Successfully Connected...")
+class DeleteDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        loadUi('Delete_dialog.ui', self)
+        self.Delete_button.clicked.connect(self.deleteRow)
+
+    def deleteRow(self):
+        name = self.Delete_row.text()
+        with pymysql.connect(host=host, port=3306, password=password, database=db_name,
+                             cursorclass=pymysql.cursors.DictCursor, user=user) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('DELETE FROM Students WHERE firstname= %s LIMIT 1', name)
+                connection.commit()
+                self.Delete_error.setText("Запись успешно удалена из БД.")
+                print("Запись успешно удалена из БД.")
+
+
+class MyForm(QDialog):
+    def __init__(self):
+        super().__init__()
+        loadUi('Add_student_form.ui', self)
+        self.Save_button.clicked.connect(self.addStudent)
+        self.connection = pymysql.connect(host=host, port=3306, password=password, database=db_name,
+                                          cursorclass=pymysql.cursors.DictCursor, user=user)
+
+    def addStudent(self):
         try:
-            cursor = connection.cursor()
-            pass
-        finally:
-            connection.close()
-    except Exception as ex:
-        print("Connection error")
-        self.Result_Area.setText("Connection Error..." + ex)
-        print(ex)
+            query_data = [self.Name.text(), self.Contact.text(), self.Cost.text(), self.Target.text(), self.Date.text()]
+            if '' in query_data:
+                self.Error_lable.setText("Ошибка: Пустые поля")
+                self.checkFields()
+            else:
+                with self.connection.cursor() as cursor:
+                    cursor.execute(
+                        'INSERT INTO students (firstname,contact,cost,target,data_add) VALUES (%s,%s,%s,%s,%s)',
+                        (query_data[0], query_data[1], int(query_data[2]), query_data[3], query_data[4]))
+                    self.connection.commit()
+                    self.Error_lable.setText("Данные успешно добавлены в БД.")
+                    print("Данные успешно добавлены в БД.")
+                    self.checkFields()
+        except Exception as ex:
+            print("Error.")
+            print(ex)
+            self.checkFields()
+
+    def checkFields(self):
+        style = "background-color: qlineargradient(spread:pad, x1:0.573864, y1:0.006, x2:0.551, y2:0.988636," \
+                "stop:0.375 rgba(141, 206, 180, 255), stop:1 rgba(255, 255, 255, 255));\nborder-radius: 10px;"
+        fields = [self.Name, self.Contact, self.Cost, self.Target, self.Date]
+        for field in fields:
+            if field.text() == "":
+                field.setStyleSheet(
+                    "background-color: qlineargradient(spread:pad, x1:0.573864, y1:0.006, x2:0.551, y2:0.988636,"
+                    "stop:0.375 rgba(255, 0, 0, 255), stop:1 rgba(163, 0, 0, 255));border-radius: 10px")
+            else:
+                field.setStyleSheet(style)
 
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
-        # Загрузка файла .ui
+        self.connection = None
         loadUi('Assistant.ui', self)
-        self.Show_Student_All.clicked.connect(self.Show_student_All_f)
-        self.Add_student.clicked.connect(self.Add_student_f)
-        self.Show_Student_Active.clicked.connect(self.Show_Student_Active_f)
-        self.Del_Student.clicked.connect(self.Del_student_f)
+        self.Show_Student_All.clicked.connect(self.showAllStudents)
+        self.Add_student.clicked.connect(self.openDialog)
+        self.Show_Student_Active.clicked.connect(self.showActiveStudents)
+        self.Del_Student.clicked.connect(self.openDeleteDialog)
+        self.model = QStandardItemModel()
+        self.tableView.setModel(self.model)
 
-    def Show_student_All_f(self):
-        new_text = "Button Show_student_All"
-        self.Result_Area.setText(new_text)
+    def openDialog(self):
+        self.dialog = MyForm()
+        self.dialog.show()
 
-    def Add_student_f(self):
-        new_text = "Button Add_student"
-        self.Result_Area.setText(new_text)
+    # def showAllStudents(self):
+    #     try:
+    #         self.connection = pymysql.connect(host=host, port=3306, password=password, database=db_name,
+    #                                           cursorclass=pymysql.cursors.DictCursor, user=user)
+    #         with self.connection.cursor() as cursor:
+    #             cursor.execute('SELECT * FROM students')
+    #             results = cursor.fetchall()
+    #         self.tableWidget.setRowCount(len(results))
+    #         for row, result in enumerate(results):
+    #             for column, data in enumerate(result.values()):
+    #                 item = QTableWidgetItem(str(data))
+    #                 self.tableWidget.setItem(row, column, item)
+    #     except Exception as ex:
+    #         print(" error")
+    #         print(ex)
 
-    def Del_student_f(self):
-        new_text = "Button Del_student"
-        self.Result_Area.setText(new_text)
+    def showAllStudents(self):
+        try:
+            self.connection = pymysql.connect(host=host, port=3306, password=password, database=db_name,
+                                              cursorclass=pymysql.cursors.DictCursor, user=user)
+            with self.connection.cursor() as cursor:
+                cursor.execute('SELECT * FROM students ')
+                results = cursor.fetchall()
 
-    def Show_Student_Active_f(self):
-        new_text = "Button Show_Student_Active"
-        self.Result_Area.setText(new_text)
+            self.model.clear()
+
+            # Устанавливаем количество строк и столбцов в модели
+            self.model.setRowCount(len(results))
+            self.model.setColumnCount(
+                len(results[0]))  # Предполагается, что все записи имеют одинаковое количество столбцов
+
+            # Заполняем модель данными из результатов запроса
+            for row, result in enumerate(results):
+                for column, (key, value) in enumerate(result.items()):
+                    item = QStandardItem(str(value))
+                    self.model.setItem(row, column, item)
+
+            # Устанавливаем заголовки столбцов
+            column_names = list(results[0].keys())
+            self.model.setHorizontalHeaderLabels(column_names)
+
+            # Разрешаем изменение размеров столбцов и выделение целых строк
+            self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+            self.tableView.setSelectionBehavior(QTableView.SelectRows)
+
+        except Exception as ex:
+            print("Ошибка при получении данных из базы данных.")
+            print(ex)
+
+    def openDeleteDialog(self):
+        self.dialog1 = DeleteDialog()
+        self.dialog1.show()
+
+    def showActiveStudents(self):
+        # TODO: Implement the functionality to show active students
+        pass
 
 
 if __name__ == '__main__':
