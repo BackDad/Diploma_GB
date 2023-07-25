1912549646352182e22a7fc5c57aa36930a489b7
BackDad <manuk_1122@mail.ru>
2023-07-25 17:45:31 +0300

    изменен порядок открытия окон

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QDialog
 from screens.mainwindow import MainWindow
+from screens.authwindow import AuthWindow
 
 if __name__ == '__main__':
+
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+    auth_window = AuthWindow()
+    if auth_window.exec_() == QDialog.Accepted:
+        window = MainWindow(auth_window.connection)
+        window.show()
+        sys.exit(app.exec_())
