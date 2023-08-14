c0c8460fd3487235e5184a540343a2d0c524fc26
BackDad <manuk_1122@mail.ru>
2023-08-14 11:59:15 +0300

    добавлено отображение занятий учеников при выборе ученика

--- screens/mainwindow.py
+++ screens/mainwindow.py
@@ -8,6 +8,7 @@ from screens.billForm import billForm
 from screens.add_lesson import Add_lesson
 from datetime import datetime
 from PyQt5.QtCore import QTimer, QTime, Qt
+from PyQt5.QtGui import QColor
 
 
 class CustomException(Exception):
@@ -41,19 +42,21 @@ class MainWindow(QMainWindow):
         self.model = QStandardItemModel()
         self.model_dolg = QStandardItemModel()
         self.model_all_without = QStandardItemModel()
-
+        self.model_lessons = QStandardItemModel()
+        self.tableView_3.setModel(self.model_lessons)
         self.tableView.setModel(self.model)
         self.tableView_2.setModel(self.model_dolg)
         self.model_dolg.dataChanged.connect(self.updateCellText)
 
         self.tableView_4.setModel(self.model_all_without)
-
+        self.tableView_4.selectionModel().selectionChanged.connect(self.show_lessons)
         self.headers = ['ID', 'Ученик', 'Номер \n телефона', 'Гонорар', 'Цель', 'Дата \n начала', 'Первый \n день',
                         'Время \n первого \n  занятия',
                         'Второй \n день', 'Время \n второго \n занятия']
 
-        self.headers_2 = ['ID', 'Ученик', 'Стоимость', 'Тема занятия', 'Дата занятия', 'Статус оплаты']
+        self.headers_2 = ['Ученик', 'Тема занятия', 'Дата занятия', 'Статус оплаты']
         self.headers_w = ['ID', 'Имя', 'Цель']
+        self.headers_lesson = ['Тема', 'Дата']
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_time)
@@ -69,15 +72,18 @@ class MainWindow(QMainWindow):
         self.bill_form.list_of_student()
         self.bill_form.studentSelected.connect(self.bill_form.list_of_lessons)
 
-        self.bill_form.data_updated.connect(self.show_dolgu)
+        self.bill_form.data_updated.connect(self.show_payed_lessons)
         self.add_lesson_w = Add_lesson(self.connection, self.bill_form)
-        self.add_lesson_w.data_updated.connect(self.show_dolgu)
+
+        # self.add_student_form.data_updated.connect(self.add_lesson_w.list_of_student)
+        self.add_lesson_w.data_updated.connect(self.show_payed_lessons)
 
         if self.Tconnection():
             self.show_all_students()
 
-        self.show_dolgu()
+        self.show_payed_lessons()
         self.show_students()
+        self.show_lessons()
 
     def open_add_student_form(self):
 
@@ -120,6 +126,7 @@ class MainWindow(QMainWindow):
     def open_bill_form(self):
         self.bill_form.show()
 
+    # TODO: переработать вывод профайла
     def show_student_info(self, index):
         # Получаем данные об ученике из модели
 
@@ -134,17 +141,20 @@ class MainWindow(QMainWindow):
         self.profile = StudentInfoDialog(student_data)
         self.profile.show()
 
+    # TODO: Убрать try exept
     def add_lesson(self):
         try:
             self.add_lesson_w.show()
         except Exception as ex:
             print(ex)
 
+    # TODO: Переработать передачу подключения к БД
     def Tconnection(self):
         with self.connection.cursor() as cursor:
             cursor.execute('SELECT * FROM students ')
         return cursor.fetchall()
 
+    # TODO: Переопределить через втроенные методы
     def update_time(self):
         current_time = QTime.currentTime()
         time_text = current_time.toString('hh:mm:ss')
@@ -178,13 +188,13 @@ class MainWindow(QMainWindow):
                 temp_s += temp_str
             self.lesson_lable.setText(temp_s)
 
-    def show_dolgu(self):
+    def show_payed_lessons(self):
 
         with self.connection.cursor() as cursor:
             # cursor.execute( 'SELECT students.id, students.firstname,students.cost FROM students JOIN lessons ON
             # students.id = lessons.students_id WHERE lessons.payment_bool = 0')
             cursor.execute(
-                'SELECT students.id, students.firstname,students.cost,lessons.lesson_topic, lessons.lesson_date,lessons.payment_bool FROM students INNER JOIN lessons ON students.id = lessons.students_id')
+                'SELECT students.firstname, lessons.lesson_topic, lessons.lesson_date,lessons.payment_bool FROM students INNER JOIN lessons ON students.id = lessons.students_id')
             results = cursor.fetchall()
             print(results)
             if self.results:
@@ -210,9 +220,8 @@ class MainWindow(QMainWindow):
                     print(ex)
 
     def show_students(self):
-
         with self.connection.cursor() as cursor:
-            cursor.execute('SELECT id, firstname, target FROM students ')
+            cursor.execute('SELECT id,firstname, target FROM students ')
         results = cursor.fetchall()
         if results:
             self.model_all_without.clear()
@@ -239,20 +248,56 @@ class MainWindow(QMainWindow):
         # self.Erro_lable.setText(f"База данных пуста {updTime()}")
 
     def updateCellText(self, topLeftIndex, bottomRightIndex):
-        for row in range(topLeftIndex.row(), bottomRightIndex.row() + 1):
-            for col in range(topLeftIndex.column(), bottomRightIndex.column() + 1):
-                index = self.model_dolg.index(row, col)
-                value = self.model_dolg.data(index, Qt.DisplayRole)
-                if value == "0":
-                    self.model_dolg.setData(index, "Не оплачено", Qt.DisplayRole)
-                    self.model_dolg.setData(index, '#FFF023', Qt.TextColorRole)
-                    self.model_dolg.setData(index, Qt.red, Qt.BackgroundRole)
-                elif value == '1':
-                    self.model_dolg.setData(index, "Оплачено", Qt.DisplayRole)
-                    self.model_dolg.setData(index, Qt.green, Qt.TextColorRole)
-                    self.model_dolg.setData(index, Qt.red, Qt.BackgroundRole)
+        try:
+            for row in range(topLeftIndex.row(), bottomRightIndex.row() + 1):
+                for col in range(topLeftIndex.column(), bottomRightIndex.column() + 1):
+                    index = self.model_dolg.index(row, col)
+                    value = self.model_dolg.data(index, Qt.DisplayRole)
+                    if value == "0":
+                        self.model_dolg.setData(index, "Не оплачено", Qt.DisplayRole)
+                        self.model_dolg.setData(index, Qt.red, Qt.TextColorRole)
+                        self.model_dolg.setData(index, QColor('#FFF023'), Qt.BackgroundRole)
+                    elif value == '1':
+                        self.model_dolg.setData(index, "Оплачено", Qt.DisplayRole)
+                        self.model_dolg.setData(index, Qt.green, Qt.TextColorRole)
+                        self.model_dolg.setData(index, Qt.white, Qt.BackgroundRole)
+        except Exception as ex:
+            print(ex)
+
+    def show_lessons(self):
+        selected_rows = self.tableView_4.selectionModel().selectedRows()
+        if selected_rows:
+            selected_row = selected_rows[0]
+            student_id = self.tableView_4.model().index(selected_row.row(), 0).data()
+            print(student_id)
+            with self.connection.cursor() as cursor:
+                cursor.execute('SELECT lesson_topic,lesson_date FROM lessons WHERE students_id = %s ', student_id)
+            results = cursor.fetchall()
+            if results:
+                self.model_lessons.clear()
+                # Устанавливаем количество строк и столбцов в модели
+                self.model_lessons.setRowCount(len(results))
+                self.model_lessons.setColumnCount(
+                    len(results[0]))  # Предполагается, что все записи имеют одинаковое количество столбцов
+                # Заполняем модель данными из результатов запроса
+                for row, result in enumerate(results):
+                    for column, (key, value) in enumerate(result.items()):
+                        item = QStandardItem(str(value))
+                        self.model_lessons.setItem(row, column, item)
+
+                self.model_lessons.setHorizontalHeaderLabels(
+                    self.headers_lesson)  # Разрешаем изменение размеров столбцов и
+                # выделение целых строк
+                self.tableView_3.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+                self.tableView_3.setSelectionBehavior(QTableView.SelectRows)
+                # self.Erro_lable.setText(f"Обновлено {updTime()} ")
+            else:
+                self.model_lessons.clear()
+                # Устанавливаем заголовки столбцов
+                # column_names= list(self.results[0].keys())
 
 
+# TODO: переопределить через стандартные методы
 def updTime():
     return f"{datetime.now().time().hour}:" \
            f"{datetime.now().time().minute}:" \
